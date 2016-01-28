c2e97559c6b3439a8571be18c5bdea0c85888d64
Rob LaFeve <rob@cremalab.com>
2016-01-27 23:57:36 -0600

    Works most of the time

--- rob.js
+++ rob.js
@@ -5,13 +5,17 @@
     floors.map(function(floor) {
       var buttonPress = 'up_button_pressed' || 'down_button_pressed'
       floor.on(buttonPress, function() {
-        elevators.
+        elevators.map(function(elevator) {
+          if(!elevator.destinationQueue.length) {
+            elevator.goToFloor(floor.level, true)
+          }
+        })
       })
     })
 
     elevators.map(function(elevator, i) {
       elevator.on('floor_button_pressed', function(floor) {
-        elevator.goToFloor(floor)
+        elevator.goToFloor(floor, true)
       })
     })
 
