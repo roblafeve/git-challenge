8dd14ed15d2942feef61ed2cdbb1fc1f1164dd68
Aaron Gerber <aarongerber@users.noreply.github.com>
2016-01-29 19:25:28 -0600

    Improved Idle elevator logic... it now goes to the closest floor to assist a passenger. Improve logic further by considering other elevators.

--- aaron.js
+++ aaron.js
@@ -3,12 +3,29 @@
 
       // Elevator Input - Send elevator to floor based on rider's input
       elevators.map(function(elevator) {
-        // If an elevator button is pressed head to that floor.
+          // If an elevator button is pressed head to that floor.
           elevator.on('floor_button_pressed', function(floor) {
               elevator.goToFloor(floor);
           })
 
-          elevator.on("idle", function() {elevator.goToFloor(0); });
+          // If elevator is idle, assign a floor.
+          elevator.on("idle", function() {
+            var Bestfloor = floors.length * 3;
+
+            floors.map(function(floor){
+              if (floor.buttonStates.up == "activated" || floor.buttonStates.down == "activated") {
+                if ( Math.abs(floor.floorNum() - elevator.currentFloor()) < Math.abs(Bestfloor - elevator.currentFloor())) {
+                  Bestfloor = floor.floorNum();
+                }
+              }
+            });
+            // Go to the closest floor
+            if (Bestfloor !== floors.length * 3) {
+              floors[Bestfloor].buttonStates.up = "";
+              floors[Bestfloor].buttonStates.down = "";
+              elevator.goToFloor(Bestfloor);
+            };
+         });
 
           // As you pass the floor stop if a button is pressed.
           elevator.on("passing_floor", function(floorNum, direction) {
